218ed37a2dcd0cd3b2e180fa10c5f06a85276086
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-03-07 20:37:57 -0500

    feat: navigate to home ofter login

--- src/app/core/controllers/user-authentication.controller.ts
+++ src/app/core/controllers/user-authentication.controller.ts
@@ -1,15 +1,19 @@
 import {Injectable} from '@angular/core';
 import {UserService} from '../services/user-service';
-import {take} from 'rxjs';
-import {UserLoginModel} from '../models/user-auth.model';
+import {take, tap} from 'rxjs';
+import {UserLoginModel, UserLoginResponse} from '../models/user-auth.model';
+import {Route, Router} from '@angular/router';
 
 @Injectable({ providedIn: "root"})
 export class UserAuthenticationController {
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,
+              private route: Router) {}
 
   userLogin(user: UserLoginModel) {
     this.userService.login(user.email, user.password)
-      .pipe(take(1)).subscribe();
+      .pipe(take(1), tap((value: UserLoginResponse) => {
+        if(value.email) this.route.navigate(['/home']);
+      })).subscribe();
   }
 }
