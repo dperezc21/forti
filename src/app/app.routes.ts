24475b726e31f415555ca311cd1348a4ff365639
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-03-12 22:14:52 -0500

    feat: home component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'auth/log-in',
+    redirectTo: 'auth/login',
     pathMatch: 'full'
   },
   {
     path: 'auth',
     loadChildren: () => import('./routes/auth-route').then(value => value.authRoute)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home.component').then(value => value.HomeComponent)
   }
 ];
